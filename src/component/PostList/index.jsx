976e6224ea51e51f175850d8b871dc8504d8a541
unknown <vietdanghoang1705@gmail.com>
2023-11-14 23:07:04 +0700

    done list post

--- src/component/PostList/index.jsx
+++ src/component/PostList/index.jsx
@@ -17,7 +17,7 @@ const fakeData = [
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
         time: "13/11/2023",
         author: "Nguyễn Văn Hiếu",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lorem"
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi lo"
     },
     {
         id: 3,
